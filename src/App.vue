13c8fb3f912a1e9a194d7fe6c557b449d2cafc1f
Colton McCurdy <mccurdyc22@gmail.com>
2019-07-02 09:05:14 -0400

    WIP; looking pretty good
    Signed-off-by: Colton McCurdy <mccurdyc22@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,90 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-toolbar app>
+      <v-toolbar-title class="headline text-titlecase">
+        <span>NaaS:</span>
+        <span class="font-weight-light"> Neighbor as a Service</span>
+      </v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn md>
+        <font-awesome-icon class="fa-fw" :icon="['fab', 'github']"/>
+        Sign In With GitHub
+      </v-btn>
+    </v-toolbar>
+
+    <v-content>
+      <Home/>
+    </v-content>
+
+  <v-footer
+      dark
+      height="auto"
+      color="primary"
+    >
+      <v-container fluid grid-list-md>
+        <v-card
+          flat
+          tile
+          color="primary"
+          class="white--text text-md-center"
+        >
+            <v-layout row justify-center>
+              <v-flex md2>
+                <v-card-title class="yellow--text title justify-center font-weight-bold">
+                  Information
+                </v-card-title>
+                <v-card dark color>
+                  <v-card-text>Pricing</v-card-text>
+                  <v-card-text>Roadmap</v-card-text>
+                </v-card>
+              </v-flex>
+
+              <v-flex md2>
+                <v-card-title class="yellow--text title justify-center font-weight-bold">
+                  Contact Us
+                </v-card-title>
+                <v-card dark color>
+                  <v-card-text>Twitter</v-card-text>
+                  <v-card-text>GitHub</v-card-text>
+                </v-card>
+              </v-flex>
+
+              <v-flex md2>
+                <v-card-title class="yellow--text title justify-center font-weight-bold">
+                  Legal
+                </v-card-title>
+                <v-card dark color=primary.darken>
+                  <v-card-text>Terms of Service</v-card-text>
+                  <v-card-text>Privacy Policy</v-card-text>
+                  <v-card-text>Subprocessors</v-card-text>
+                </v-card>
+              </v-flex>
+            </v-layout>
+
+          <v-card-text class="white--text">
+            &copy;2019 — <strong>Neighbor</strong>
+          </v-card-text>
+        </v-card>
+      </v-container>
+    </v-footer>
+  </v-app>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/Home'
 
 export default {
-  name: 'app',
+  name: 'App',
   components: {
-    HelloWorld
+    Home
+  },
+  data () {
+    return {
+      //
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,34 @@
 import Vue from 'vue'
+import Vuetify from 'vuetify'
 import App from './App.vue'
+import 'vuetify/dist/vuetify.min.css'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCode, faCodeBranch, faFileCode, faFileContract, faFile, faFileAlt, faList, faCheck, faSearch } from '@fortawesome/free-solid-svg-icons'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faGithub, faCode, faCodeBranch, faFileCode, faFileContract, faFile, faFileAlt, faList, faCheck, faSearch)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+Vue.use(Vuetify, {
+  iconfont: 'fa',
+  theme: {
+    primary: {
+      base: "#515151",
+      darken1: "#2D2D2D"
+    },
+    secondary: {
+      base: "#F2F0EC",
+      darken1: "#D3D0C8"
+    },
+    red: "#F2777A",
+    yellow: "#FFCC66",
+    blue: "#6699CC",
+    purple: "#CC99CC",
+    teal: "#66CCCC"
+  }
+})
 
 Vue.config.productionTip = false
 
