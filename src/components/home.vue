cb68f6c2470a093b805fecf92e1f0519dc010136
Colton McCurdy <mccurdyc22@gmail.com>
2019-07-04 12:49:55 -0400

    WIP: spacing looks good
    Signed-off-by: Colton McCurdy <mccurdyc22@gmail.com>

--- src/components/home.vue
+++ src/components/home.vue
@@ -5,23 +5,23 @@ Vue.use(Vuetify)
 <template>
   <v-app>
     <v-content>
-      <v-layout justify-center>
-        <v-flex xs12 sm6>
-          <v-container fluid grid-list-md >
-            <v-layout row wrap>
+      <v-layout>
+        <v-flex md12 offset-md2>
+          <v-container fluid fill-height grid-list-md>
+            <v-layout row>
               <v-flex
                 v-for="action in actions"
                 :key="action.title"
-                v-bind="{ [`sm${action.flex}`]: true }"
+                v-bind="{ [`md${action.flex}`]: true }"
               >
                 <v-card>
-                  <v-container grid-list-md>
-                    <v-layout row wrap>
-                      <v-flex pa-3 ma-1>
+                  <v-container fluid grid-list-md>
+                    <v-layout align-center column>
+                      <v-flex ma-3>
                         <font-awesome-icon class="fa-10x" :icon="[`${action.icon_group}`, `${action.icon}`]"/>
                       </v-flex>
 
-                      <v-flex>
+                      <v-flex ma-3>
                         <v-card-title>
                           <div>
                             <h2>{{ action.title }}</h2>
@@ -54,7 +54,8 @@ Vue.use(Vuetify)
     name: 'home',
     data: () => ({
       actions: [
-        { title: 'Add CODEOWNERs File', icon_group: 'fas', icon: 'file', flex: 6, description: 'Add a CODEOWNERs file to the root of your repositories.'},
+        { title: 'Add CODEOWNERs File', icon_group: 'fas', icon: 'file', flex: 5, description: 'Add a CODEOWNERs file to the root of your repositories.'},
+        { title: 'Add CODEOWNERs File', icon_group: 'fas', icon: 'file', flex: 5, description: 'Add a CODEOWNERs file to the root of your repositories.'},
       ]
     })
   }
@@ -62,37 +63,4 @@ Vue.use(Vuetify)
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-font-awesome-icon {
-  font-size: 50px;
-}
-
-.container {
-  margin: 100px;
-}
-
-.font-awesome-icon{
-  margin: 50px;
-}
-button {
-  text-align: center;
-  text-transform: capitalize;
-  display: inline-block;
-  font-size: 30px;
-  padding: 50px 100px 250px 100px;
-  border-radius: 5px;
-}
 </style>
