f0dcef54669ae351f9d6b811d7d4755708125641
Colton McCurdy <mccurdyc22@gmail.com>
2019-07-06 14:11:55 -0400

    seems to be formatting nicely...
    Signed-off-by: Colton McCurdy <mccurdyc22@gmail.com>

--- src/components/home.vue
+++ src/components/home.vue
@@ -13,33 +13,39 @@ Vue.use(Vuetify)
                 v-for="action in actions"
                 :key="action.title"
                 v-bind="{ [`md${action.flex}`]: true }"
+                d-flex
               >
-                <v-card :color="`${action.color}`" light>
-                  <v-container fluid grid-list-md>
-                    <v-layout align-center column>
-                      <v-flex ma-1>
-                        <font-awesome-icon class="fa-10x" :icon="[`${action.icon_group}`, `${action.icon}`]"/>
-                      </v-flex>
 
-                      <v-flex ma-1>
-                        <v-card-title>
-                          <div>
-                            <h2>{{ action.title }}</h2>
-                            <div>{{ action.description }}</div>
-                          </div>
-                        </v-card-title>
-                      </v-flex>
+                <v-card :color="`${action.color}`" light>
+                  <v-container grid-list-md class="container-header">
+                    <v-layout column>
+                      <v-layout align-center column ma-2>
+                        <font-awesome-icon class="fa-8x" :icon="[`${action.icon_group}`, `${action.icon}`]"/>
+                      </v-layout>
+
+                      <v-layout align-center column ma-2>
+                        <div>
+                          <h2>{{ action.title }}</h2>
+                        </div>
+                      </v-layout>
                     </v-layout>
                   </v-container>
 
-                  <v-card-actions>
-                    <v-spacer></v-spacer>
-                    <v-btn>
-                      Run
-                    </v-btn>
-                  </v-card-actions>
+                  <v-container grid-list-md class="container-body">
+                      <v-card-text class="container-body">
+                        <div>
+                          {{ action.description }}
+                        </div>
+                      </v-card-text>
 
+                      <v-card-actions>
+                        <v-btn>
+                          Run
+                        </v-btn>
+                      </v-card-actions>
+                  </v-container>
                 </v-card>
+
               </v-flex>
             </v-layout>
           </v-container>
@@ -55,7 +61,7 @@ Vue.use(Vuetify)
     data: () => ({
       actions: [
         { title: 'Add CODEOWNERs File', color: 'yellow', icon_group: 'fas', icon: 'file', flex: 5, description: 'Add a CODEOWNERs file to the root of your repositories.'},
-        { title: 'Add LICENSE File', color: 'red', icon_group: 'fas', icon: 'file-contract', flex: 5, description: 'Add a CODEOWNERs file to the root of your repositories.'},
+        { title: 'Add LICENSE File', color: 'red', icon_group: 'fas', icon: 'file-contract', flex: 5, description: 'Add one of supported licenses (e.g., MIT, GNU General Public License [v2.0, v3.0], Apache license 2.0) to the root of your repositories as a LICENSE.[txt, md] file.'},
         { title: 'List Dependencies', color: 'blue', icon_group: 'fas', icon: 'list', flex: 5, description: 'Add a CODEOWNERs file to the root of your repositories.'},
         { title: 'Find and Replace Text', color: 'teal', icon_group: 'fas', icon: 'search', flex: 5, description: 'Add a CODEOWNERs file to the root of your repositories.'},
       ]
@@ -65,4 +71,21 @@ Vue.use(Vuetify)
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.v-card {
+  position: relative;
+}
+
+.v-card__actions {
+  position: absolute;
+  bottom: 0px;
+  right:0px;
+}
+
+.container-header {
+  height: 50%;
+}
+
+.container-body {
+  height: 50%;
+}
 </style>
